3e50eb0cba0756199c27202c392c3e57f40a0bf5
Joy879 <joywanjiru879@gmail.com>
2021-08-31 06:41:46 -0700

    _Putchar

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,13 +6,15 @@
  */
 int main(void)
 {
-	char *c = "_putchar";
+	char c[] = "_putchar";
 	int i = 0;
 
-	for (i = 0; i < 10; i++)
+	while (c[i] != '\0')
 	{
 		_putchar(c[i]);
+		i++;
 	}
 	_putchar('\n');
+
 	return (0);
 }
