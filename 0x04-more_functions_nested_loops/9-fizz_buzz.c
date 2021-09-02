3ff7f8838a403479ba6d5a31e5e5fdcf91676cb1
Joy879 <joywanjiru879@gmail.com>
2021-09-02 09:49:55 -0700

    Fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,11 +11,11 @@ int main(void)
 	for (i = 1; 1 <= 100; i++)
 	{
 		if (i % 15 == 0)
-			printf("FizzBuzz");
+			printf("FizzBuzz\t");
 		else if ((i % 3) == 0)
-			printf("Fizz");
+			printf("Fizz\t");
 		else if ((i % 5) == 0)
-			printf("Buzz");
+			printf("Buzz\t");
 		else
 			printf("%d\t", i);
 	}
