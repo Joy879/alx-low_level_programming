e66b80db595140f5a6b76bb73299768b45cc64b1
Joy879 <joywanjiru879@gmail.com>
2021-09-13 13:18:28 -0700

    Chessis mental torture

---
+++ 0x07-pointers_arrays_strings/7-chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_chessboard - prints a chessboard
+ * @: array of pointers
+ * Return: void
+ */
+void print_chessboard(char (*a)[8])
+{
+	int x, y;
+
+	for (x = 0; x <= 7; x++)
+	{
+		for (y = 0; y <= 7; y++)
+		{
+			_putchar(a[x][y]);
+		}
+		_putchar('\n');
+	}
+}
