70b367c61d0fa4ca3637aa80a4c9e7e3a3204ec2
Joy879 <joywanjiru879@gmail.com>
2022-03-29 05:42:09 -0700

    Search Algorithms

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,5 +8,9 @@
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 void print_arr(int *array, size_t l, size_t r);
+int jump_search(int *array, size_t size, int value);
+int binary_search_helper(int *array, size_t left, size_t right, int value);
+int advanced_binary(int *array, size_t size, int value);
+
 
 #endif /*SEARCH_ALGOS*/
